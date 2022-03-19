ca44761f4dac667da41186501023f79fd982ef78
Pasit Sri-intarasut <ong.pasit@gmail.com>
2022-03-19 15:31:13 +0700

    feat: add thai lang in word manager

--- WordManager.py
+++ WordManager.py
@@ -1,5 +1,8 @@
 import nltk
 from nltk.corpus import stopwords
+from pythainlp.tokenize import word_tokenize
+from pythainlp.tag import pos_tag
+from pythainlp.corpus import thai_stopwords
 import re
 class WordManager:
 
@@ -7,8 +10,23 @@ class WordManager:
         tokenized = nltk.word_tokenize(content)
         nouns = [word for (word, pos) in nltk.pos_tag(tokenized) if pos == "NNP"] 
         return nouns
+
+    def get_nouns_th(self, content):
+        text = word_tokenize(content, engine="attacut")
+        noun_type = ['PRON', 'PROPN', 'NOUN', 'NPRP', 'NTTL', 'NCNM']
+        nouns = [word for (word, pos) in pos_tag(text, corpus='pud') if word!=" " and pos in noun_type]
+        return nouns
     
-    def clear_text(self, text):
+    def clean_text_th(self, text):
+        text = text.lower()
+        thai_stopword = set(thai_stopwords())
+        token = word_tokenize(text, engine='attacut')
+        text_list = [t for t in token if t not in thai_stopword]
+        text = " ".join(text_list)
+        text = self.remove_url_th(text)
+        return text
+
+    def clean_text(self, text):
         text = text.lower()
         eng_stopword = set(stopwords.words('english'))
         token = nltk.word_tokenize(text)
@@ -52,3 +70,10 @@ class WordManager:
         """
 
         return " ".join(re.sub("([^\u0E00-\u0E7Fa-zA-Z' ]|^'|'$|''|(\w+:\/\/\S+))", "", txt).split())
+
+if __name__ == '__main__':
+    w = WordManager()
+    text = "ทดสอบระบบครับ พสิษฐ์ทำจะแบบนี้ทำไมครับ โรนัลโด้เก่งมาก"
+    text = w.clear_text_th(text)
+    n = w.get_nouns_th(text)
+    print(n)

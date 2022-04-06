e35a2f800a66881190835810554d26e754b79cda
Pasit Sri-intarasut <ong.pasit@gmail.com>
2022-04-07 01:02:05 +0700

    feat: add keyword in gui

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ class MainWindow(QMainWindow):
         self.ui.SearchList.itemDoubleClicked.connect(self.selectKeywordsData)
         self.ui.base_table.doubleClicked.connect(self.openLink)
         self.ui.pushButton.clicked.connect(self.filterDomainPopup)
+        self.ui.searchButton.clicked.connect(self.searchKeyword)
 
     def updateKeywords(self):
         self.keywords = self.keyword_manager.get_all_keywords()
@@ -33,6 +34,17 @@ class MainWindow(QMainWindow):
         self.ui.base_table.setColumnWidth(0, 200)
         self.ui.base_table.setColumnWidth(1, 200)
     
+    def searchKeyword(self):
+        self.keyword = self.ui.Search_LineEdit.text()
+        self.data = self.keyword_manager.search_keyword(self.keyword)
+        self.showTable()
+        self.ui.status_base_label.setText(f"{len(self.data.index)} links")
+        domains_list = self.keyword_manager.get_domain(self.keyword)
+        self.current_domains = {name: False for name in domains_list}
+        self.ui.base_label.setText(self.keyword)
+        self.updateKeywords()
+        self.ui.Search_LineEdit.clear()
+        
     def selectKeywordsData(self, item):
         self.keyword = item.text()
         self.data = self.keyword_manager.search_keyword(self.keyword)
@@ -40,6 +52,7 @@ class MainWindow(QMainWindow):
         self.ui.status_base_label.setText(f"{len(self.data.index)} links")
         domains_list = self.keyword_manager.get_domain(self.keyword)
         self.current_domains = {name: False for name in domains_list}
+        self.ui.base_label.setText(self.keyword)
    
     def openLink(self, item):
         for index in self.ui.base_table.selectionModel().selectedIndexes():

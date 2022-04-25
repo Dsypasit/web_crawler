54e88e4249c3c18af028d056bd99782f677d92a5
Pasit Sri-intarasut <ong.pasit@gmail.com>
2022-04-25 19:58:36 +0700

    feat: add selenium scrap

--- Crawler.py
+++ Crawler.py
@@ -6,8 +6,11 @@ from concurrent.futures import as_completed
 from urllib.parse import unquote, urlparse
 import urllib.robotparser
 from copy import deepcopy
-from time import time
 import numpy as np
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import os
+import time
 
 class Crawler:
     def __init__(self):
@@ -19,6 +22,10 @@ class Crawler:
         self.regex = ""
         self.regex = re.compile("")
         self._before_filter_links : list = []
+        self.is_scroll = False
+        self.is_clicked = False
+        self.id_click = []
+        self.by = None
         self.session()
 
     def read_robot(self, link):
@@ -96,12 +103,19 @@ class Crawler:
         return sub.copy()
 
     def get_all_links(self):
-        start = time()
+        start = time.time()
         url = self.url
         self.rp = self.read_robot(url)
         self._before_filter_links = []
-        sub_l = self.get_sublink(url)
-        sub_l = self.filter_links(links=sub_l)
+        if self.is_scroll == True:
+            content = self.scroll_down(url)
+            sub_l = self.get_url_links(content)
+        elif self.is_clicked == True:
+            content = self.click_button(url)
+            sub_l = self.get_url_links(content)
+        else:
+            sub_l = self.get_sublink(url)
+        # sub_l = self.filter_links(links=sub_l)
         count = 5
         while sub_l == [] and count > 0:
             sub_l = self.get_sublink(url)
@@ -119,7 +133,7 @@ class Crawler:
         self._before_filter_links = self.filter_links()
         self.links = pd.DataFrame({'url':self._before_filter_links})
         self.links.drop_duplicates(subset=['url'], inplace=True)
-        print(url, "success", time()-start)
+        print(url, "success", time.time()-start)
         return self.links['url'].values
 
     def add_n_gram(self, url):
@@ -135,6 +149,53 @@ class Crawler:
     def check_url(self, url):
         url_parse = urlparse(url)
         return all([url_parse.scheme, url_parse.netloc])
+    
+    def scroll_down(self, url):
+        path = 'webdriver/'
+        # os.chdir(path)
+        driver = webdriver.Chrome(path+"chromedriver.exe")
+        driver.get(url)
+        SCROLL_PAUSE_TIME = 0.5
+
+        # Get scroll height
+        last_height = driver.execute_script("return document.body.scrollHeight")
+        c = 0
+
+        while c<6:
+            # Scroll down to bottom
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+            # Wait to load page
+            time.sleep(SCROLL_PAUSE_TIME)
+
+            # Calculate new scroll height and compare with last scroll height
+            new_height = driver.execute_script("return document.body.scrollHeight")
+            if new_height == last_height:
+                break
+            last_height = new_height
+            c += 1
+        html = driver.page_source
+        return html
+    
+    def click_button(self, url):
+        path = 'webdriver/'
+        # os.chdir(path)
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--disable-notifications")
+        driver = webdriver.Chrome(path+"chromedriver.exe", chrome_options=chrome_options)
+        driver.get(url)
+        SCROLL_PAUSE_TIME = 1
+        c = 0
+        while c<6:
+            try:
+                driver.find_element(self.by, ' '.join(self.id_click)).click()
+                time.sleep(SCROLL_PAUSE_TIME)
+                c+= 1
+            except:
+                break
+        html = driver.page_source
+        return html
+
 
 class GoalCrawler(Crawler):
     def __init__(self):
@@ -291,14 +352,62 @@ class SportBibleCrawler(Crawler):
         self.regex = re.compile(r'(https\:\/\/www\.sportbible.com\/football\/.*\d{6,})')
         self.no_domain = True
 
+class EspnCrawler(Crawler):
+    def __init__(self):
+        super().__init__()
+        self.url = "https://global.espn.com/football/"
+        self.regex = re.compile(r'(https\:\/\/global.espn.com\/football\/*)')
+        self.no_domain = True
+        self.is_scroll = True
+
+class NdtvCrawler(Crawler):
+    def __init__(self):
+        super().__init__()
+        self.url = "https://sports.ndtv.com/football/news"
+        self.regex = re.compile(r'(https\:\/\/sports.ndtv.com\/football\/*)')
+        self.no_domain = True
+        self.is_clicked = True
+        self.by = By.ID
+        self.id_click = ['loadmorebtn']
+
+class SuperSportCrawler(Crawler):
+    def __init__(self):
+        super().__init__()
+        self.url = "https://supersport.com/football/news/"
+        self.regex = re.compile(r'(https\:\/\/supersport.com\/football\/*)')
+        self.no_domain = True
+        self.is_clicked = True
+        self.by = By.CLASS_NAME
+        self.id_click = ['button-load-more']
+
+class CollegeCrawler(Crawler):
+    def __init__(self):
+        super().__init__()
+        self.url = "https://collegefootballnews.com/"
+        self.regex = re.compile(r'(https\:\/\/collegefootballnews.com\/list\/*)')
+        self.is_scroll = True
+
+class CbssCrawler(Crawler):
+    def __init__(self):
+        super().__init__()
+        self.url = "https://www.cbssports.com/nfl/"
+        self.regex = re.compile(r'(https\:\/\/www.cbssports.com\/nfl\/news\/*)')
+        self.no_domain = True
+        self.is_clicked = True
+        self.by = By.CLASS_NAME
+        self.id_click = ['button-type-lines ']
+        
+
 
 if __name__ == "__main__":
     def time_spent(func, *args, **kwargs):
-        start = time()
+        start = time.time()
         result = func(*args, **kwargs)
-        print(time()-start)
+        print(time.time()-start)
         return result
     pd.options.display.max_colwidth = 600
-    crawler = TPBSCrawler()
+    crawler = CbssCrawler()
+    # crawler = CollegeCrawler()
     data = time_spent(crawler.get_all_links)
     print(data[:21])
+    print(len(data))
